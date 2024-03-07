3c8396fc246ad28756f6fd67ff4c00d8a5b60e74
skepch1k <148620329+skepch1k@users.noreply.github.com>
2024-03-07 14:23:04 +0300

    Add files via upload

---
+++ 11var_Skopin.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public class Podrazdelenie
+{
+    private string name;
+    private Employee head;
+    private Organization organization;
+
+    public Podrazdelenie(string name, Employee head, Organization organization)
+    {
+        this.name = name;
+        this.head = head;
+        this.organization = organization;
+    }
+
+    public string GetName()
+    {
+        return name;
+    }
+
+    public Employee GetHead()
+    {
+        return head;
+    }
+
+    public Organization GetOrganization()
+    {
+        return organization;
+    }
+}
