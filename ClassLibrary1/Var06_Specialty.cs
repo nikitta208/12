36dc9ec1d14095bf7b7e19761ff99f450444e1bb
Zanosik <148620284+Zanosik@users.noreply.github.com>
2024-03-10 21:33:43 +0300

    Add files via upload

---
+++ ClassLibrary1/Var06_Specialty.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public class Specialty
+{
+    private string name;
+    private string sokrash;
+    private DateTime date;
+
+    public Specialty(string name, string sokra, DateTime? date = null)
+    {
+        name = name;
+        sokra = sokra;
+        date = date ?? DateTime.Today;
+    }
+
+    public string Name()
+    {
+        return name;
+    }
+
+    public string Sokr()
+    {
+        return sokrash;
+    }
+
+    public DateTime Date()
+    {
+        return date;
+    }
+}
