a33c1ec3b7471fd308ae43042350f2d100207e33
MifitoMori <160855037+MifitoMori@users.noreply.github.com>
2024-03-10 23:58:03 +0300

    Update Var06_Specialty.cs

--- ClassLibrary1/Var06_Specialty.cs
+++ ClassLibrary1/Var06_Specialty.cs
@@ -5,24 +5,24 @@ public class Var06_Specialty
     private string name;
     private string sokrash;
     private DateTime date;
-
-    public Specialty(string name, string sokra, DateTime? date = null)
+    
+    public Var06_Specialty(string name, string sokra, DateTime? date = null)
     {
         name = name;
         sokra = sokra;
         date = date ?? DateTime.Today;
     }
-
+    
     public string Name()
     {
         return name;
     }
-
+    
     public string Sokr()
     {
         return sokrash;
     }
-
+    
     public DateTime Date()
     {
         return date;
