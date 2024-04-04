87fa36c1c4f1ce2217b61523da9587d02323681e
MifitoMori <160855037+MifitoMori@users.noreply.github.com>
2024-04-04 13:40:38 +0300

    Update Var10_Post.cs

--- ClassLibrary1/Var10_Post.cs
+++ ClassLibrary1/Var10_Post.cs
@@ -23,18 +23,10 @@ namespace ClassLibrary1
             set => Salary = value;
         }
 
-        private Var11_Discipline division;
-        
-        public string Division
-        {
-            get { return division.ToString(); }
-            set { division = Var11_Discipline.ToString(value); }
-        }
         private Var10_Post(string Name, string salary,  string division)
         {            
             name = Name;
             Salary = salary;
-            Division = division;
         }
         internal static Var10_Post ToString(string value)
         {
