22385c5f5190022259244c23222c64a2990fb1e4
nikitta208 <148620374+nikitta208@users.noreply.github.com>
2024-05-02 14:18:54 +0300

    Update Var07_Couple.cs

--- ClassLibrary1/Var07_Couple.cs
+++ ClassLibrary1/Var07_Couple.cs
@@ -6,10 +6,8 @@ using System.Threading.Tasks;
 
 namespace ClassLibrary1
 {
-    
     internal class Var07_Couple
     {
-
         internal static Var07_Couple ToString(string value)
         {
             throw new NotImplementedException();
