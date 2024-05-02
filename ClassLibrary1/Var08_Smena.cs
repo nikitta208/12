60b75aac82ed80f2f83577e2baf23522a53fbff0
nikitta208 <148620374+nikitta208@users.noreply.github.com>
2024-05-02 14:07:01 +0300

    Update Var08_Smena.cs

--- ClassLibrary1/Var08_Smena.cs
+++ ClassLibrary1/Var08_Smena.cs
@@ -9,17 +9,13 @@ namespace ClassLibrary1
     internal class Var08_Smena
     {
         private string name;
-
         public Var08_Smena(string name)
         {
             this.name = name;
         }
-
         public override string ToString()
         {
             return $"Название смены: {name}";
         }
-
     }
-
 }
