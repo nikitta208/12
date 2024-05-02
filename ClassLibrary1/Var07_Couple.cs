64367534fa15c00eb9ab6b1a391bd5cbc8349046
nikitta208 <148620374+nikitta208@users.noreply.github.com>
2024-05-02 14:12:10 +0300

    Update Var07_Couple.cs

--- ClassLibrary1/Var07_Couple.cs
+++ ClassLibrary1/Var07_Couple.cs
@@ -4,9 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ClassLibrary1{
+namespace ClassLibrary1
+{
     
-    internal class Var07_Couple{
+    internal class Var07_Couple
+    {
 
         internal static Var07_Couple ToString(string value)
         {
